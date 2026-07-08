3a3d6c89ff202414e60474cc2beff282e8bec78d
Milica Stanojlovic <mstanojlovic222@gmail.com>
2026-07-08 23:38:49 +0200

    Initial commit

---
+++ src/main/java/com/project/drone_missions/DroneMissionsApplication.java
@@ -0,0 +1,13 @@
+package com.project.drone_missions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneMissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneMissionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/drone_missions/DroneMissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.drone_missions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneMissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
